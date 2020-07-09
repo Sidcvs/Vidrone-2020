426766eeece3dfdad80f96bb3df23aac4f4beedd
akshat14714 <akshat14714@gmail.com>
2020-07-09 08:23:12 +0530

    Updated the conversion file

--- convertToCoco.py
+++ convertToCoco.py
@@ -3,31 +3,22 @@ import pprint
 import cv2
 import glob
 
-filename = './cocoAnnotations/instances_val2017.json'
 data = {}
-with open(filename) as inpJson:
-    data = json.load(inpJson)
 
 categoriesData = {}
 with open('initialAnnotations.json') as inpJson:
     categoriesData = json.load(inpJson)
 
-# del data['annotations']
-del data['licenses']
-del data['info']
-
-# data['annotations'] = []
-
 imageId = {}
 
-allImgs = glob.glob("images/*.jpg")
+allImgs = glob.glob("../VisDrone2019-DET-val/images/*.jpg")
 images = []
 idImg = 1
 for img in allImgs:
     newDict = {}
-    newDict['file_name'] = img.split('/')[1]
+    newDict['file_name'] = img.split('/')[-1]
     newDict['id'] = idImg
-    imageId[img.split('/')[1].split('.')[0]] = idImg
+    imageId[img.split('/')[-1].split('.')[0]] = idImg
     myimg = cv2.imread(img)
     height, width, channels = myimg.shape
     newDict['height'] = height
@@ -35,7 +26,7 @@ for img in allImgs:
     images.append(newDict)
     idImg += 1
 
-allAnnotations = glob.glob("annotations/*.txt")
+allAnnotations = glob.glob("../VisDrone2019-DET-val/annotations/*.txt")
 annotations = []
 idx = 1
 for annotation in allAnnotations:
@@ -43,7 +34,7 @@ for annotation in allAnnotations:
     for line in fp:
         newDict = {}
         newDict['id'] = idx
-        newDict['image_id'] = imageId[annotation.split('/')[1].split('.')[0]]
+        newDict['image_id'] = imageId[annotation.split('/')[-1].split('.')[0]]
         dat = line.split(',')
         newDict['category_id'] = int(dat[5]) + 1
         newDict['bbox'] = [int(dat[0]), int(dat[1]), int(dat[2]), int(dat[3])]
@@ -58,7 +49,7 @@ data['annotations'] = annotations
 
 print("Writing the Data to file...")
 
-outfile = 'instances.json'
+outfile = '../coco/instances_val2017.json'
 outdata = json.dumps(data, indent=4)
 with open(outfile, 'w') as outJson:
     outJson.write(outdata)
